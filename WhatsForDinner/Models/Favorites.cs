b02f0286fdc94f8aee557b0ea01556566a20bbd8
btopo <bret.topo@gmail.com>
2022-09-15 21:11:22 -0400

    adding models and some components

--- WhatsForDinner/Models/Favorites.cs
+++ WhatsForDinner/Models/Favorites.cs
@@ -2,6 +2,8 @@
 {
     public class Favorites
     {
-        
+        public int userid { get; set; }
+        public int favoriteid { get; set; }
+        public int recipeid { get; set; }
     }
 }
