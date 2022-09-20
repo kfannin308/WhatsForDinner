20e900e3a17021eb2eb227eb867a2f1219229360
hyaso <hyassine21@gmail.com>
2022-09-19 22:11:45 -0400

    Updated Connection string

---
+++ WhatsForDinner/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WhatsForDinner.Models;
+
+namespace WhatsForDinner.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("test")]
+        public Users TestMethod()
+        {
+            Users users = new Users();
+            users.userId = 1;
+            users.email = "dominionsucks@gmail.com";
+            users.firstName = "Dominion";
+            users.lastName = "Burch";
+            users.numberToFeed = 1;
+            
+            return users;
+        }
+    }
+}
--- WhatsForDinner/Models/Users.cs
+++ WhatsForDinner/Models/Users.cs
@@ -1,7 +1,10 @@
-﻿namespace WhatsForDinner.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhatsForDinner.Models
 {
     public class Users
     {
+        [Key]
         public int userId { get; set; }
         public string email { get; set; } = null!;
         public string firstName { get; set; } = null!;
